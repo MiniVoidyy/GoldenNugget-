8d3ddf8cbf0808cf81e12ab2477f2f4ec94e3340
awesomenull <idrustamgames@gmail.com>
2026-08-10 22:25:24 +0300

    how i frogot to bump version

--- src/gui/main_window.py
+++ src/gui/main_window.py
@@ -21,7 +21,7 @@ from src.restore.bookrestore import BookRestoreFileTransferMethod, BookRestoreAp
 
 from src.tweaks.tweaks import tweaks, TweakID
 
-App_Version = "8.0"
+App_Version = "8.1"
 App_Build = 0
 
 class MainWindow(QtWidgets.QMainWindow):
